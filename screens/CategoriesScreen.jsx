6262e92458b5d9c3dd9b752aa4088e8b4eedcb58
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-04-26 15:08:42 -0300

    categories

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTitle from '../components/CategoryGridTitle';
+
+const renderCategoryItem = itemData => {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={item => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
